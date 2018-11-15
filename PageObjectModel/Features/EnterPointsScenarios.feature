8585aea5b6045b14c8d9ec8d4fccb514bc99ae79
Jordan Enwright <jordanenwright@gmail.com>
2018-11-15 10:19:22 -0600

    new work

--- PageObjectModel/Features/EnterPointsScenarios.feature
+++ PageObjectModel/Features/EnterPointsScenarios.feature
@@ -2,5 +2,17 @@
 	these are the enter point scenarios to the website
 
 @Debug
-Scenario: Access main enter point
+Scenario: Validate the title of the website
 	Given I navigate to the homepage
+	Then I see the page title contains "Wiki"
+
+@Debug
+Scenario: Validate the URL of the website
+	Given I navigate to the homepage
+	Then I see the page url contains "Wiki"
+
+	@Debug
+Scenario: Validate the pageSource string on the website
+	Given I navigate to the homepage
+	Then I see "The Free Encyclopdia" in the PageSource
+
--- PageObjectModel/Pages/BasePage.cs
+++ PageObjectModel/Pages/BasePage.cs
@@ -1,4 +1,6 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
 using static PageObjectModel.Utils.Selenium.Driver;
 
 namespace PageObjectModel.Pages
@@ -6,9 +8,37 @@ namespace PageObjectModel.Pages
     public class BasePage :Page
     {
         public IWebDriver Driver { get; internal set; }
+        public string GetTitle => Driver.Title;
+        public string GetUrl => Driver.Url;
+        public string GetText => Driver.PageSource;
+
+
+
+        public void ValidateTextInPageSource(string expectedText)
+        {
+            var textToValidate = Driver.Title.Contains(expectedText);
+            Assert.IsTrue(textToValidate, ":: This is not the expected url");
+            Console.WriteLine("::the text {0} in the PageSource is", GetText);
+        }
+
+        public void ValidatePageUrl(string expectedUrl)
+        {
+            var urlToValidate = Driver.Title.Contains(expectedUrl);
+            Assert.IsTrue(urlToValidate, ":: This is not the expected url");
+            Console.WriteLine("::the title of the website is" + GetUrl);
+        }
+
+
+        public void ValidatePageTitle(string expectedTitle)
+        {
+            var titleToValidate = Driver.Title.Contains(expectedTitle);
+            Assert.IsTrue(titleToValidate, ":: This is not the expected title");
+            Console.WriteLine("::the title of the website is" + GetTitle);
+        }
 
         public void NavigateMainEnterPoint()
         {
+            
             const string url = "https://www.wikipedia.org/";
             Browser().Navigate().GoToUrl(url);
             Browser().Manage().Window.Maximize();
